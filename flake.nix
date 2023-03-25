121b00037df20a4daa4143589285b8d2139ba189
luqmanishere <luqmanulhakim1720@gmail.com>
2023-03-25 16:34:47 +0800

    nix: add devshell commands

--- flake.nix
+++ flake.nix
@@ -8,92 +8,115 @@
     devshell.url = "github:numtide/devshell";
   };
 
-  outputs =
-    inputs @ { parts
-    , nci
-    , devshell
-    , nixpkgs
-    , ...
-    }:
-    parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      imports = [ nci.flakeModule parts.flakeModules.easyOverlay devshell.flakeModule ];
-      perSystem = { config, pkgs, system, inputs', ... }:
-        let
-          crateName = "wayper";
-          # shorthand for accessing this crate's outputs
-          # you can access crate outputs under `config.nci.outputs.<crate name>` (see documentation)
-          crateOutputs = config.nci.outputs.${crateName};
-          libPath = with pkgs; lib.makeLibraryPath
-            [
-              libGL
-              libxkbcommon
-              wayland
-              xorg.libX11
-              xorg.libXcursor
-              xorg.libXi
-              xorg.libXrandr
-
-            ];
-        in
-        {
-          # declare projects
-          # relPath is the relative path of a project to the flake root
-          # TODO: change this to your crate's path
-          nci.projects.${crateName}.relPath = "";
-          # configure crates
-          nci.crates.${crateName} = {
-            # export crate (packages and devshell) in flake outputs
-            # alternatively you can access the outputs and export them yourself (see below)
-            export = true;
-            # look at documentation for more options
-            overrides = {
-              add-inputs.overrideAttrs = old: {
-                nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.wayland-protocols pkgs.makeWrapper ];
-                buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.pkg-config pkgs.openssl.dev pkgs.openssl pkgs.perl ];
-                postInstall = ''
-                  wrapProgram "$out/bin/wayper" --prefix LD_LIBRARY_PATH : "${libPath}"
-                '';
-              };
-            };
-            depsOverrides = {
-              add-inputs.overrideAttrs = old: {
-                nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.wayland-protocols ];
-                buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.pkg-config pkgs.openssl.dev pkgs.openssl pkgs.perl ];
-              };
+  outputs = inputs @ {
+    parts,
+    nci,
+    devshell,
+    ...
+  }:
+    parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [nci.flakeModule parts.flakeModules.easyOverlay devshell.flakeModule];
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        inputs',
+        ...
+      }: let
+        crateName = "wayper";
+        # shorthand for accessing this crate's outputs
+        # you can access crate outputs under `config.nci.outputs.<crate name>` (see documentation)
+        crateOutputs = config.nci.outputs.${crateName};
+        libPath = with pkgs;
+          lib.makeLibraryPath
+          [
+            libGL
+            libxkbcommon
+            wayland
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+          ];
+      in {
+        # declare projects
+        # relPath is the relative path of a project to the flake root
+        # TODO: change this to your crate's path
+        nci.projects.${crateName}.relPath = "";
+        # configure crates
+        nci.crates.${crateName} = {
+          # export crate (packages and devshell) in flake outputs
+          # alternatively you can access the outputs and export them yourself (see below)
+          export = true;
+          # look at documentation for more options
+          overrides = {
+            add-inputs.overrideAttrs = old: {
+              nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.wayland-protocols pkgs.makeWrapper];
+              buildInputs = (old.buildInputs or []) ++ [pkgs.pkg-config pkgs.openssl.dev pkgs.openssl pkgs.perl];
+              postInstall = ''
+                wrapProgram "$out/bin/wayper" --prefix LD_LIBRARY_PATH : "${libPath}"
+              '';
             };
           };
-
-          devshells.default = with pkgs;{
-            motd = ''
-              -----------------
-              -wayper devshell-
-              -----------------
-              $(type -p menu &>/dev/null && menu)
-            '';
-            env = [
-              { name = "RUST_SRC_PATH"; value = rustPlatform.rustLibSrc; }
-              { name = "LD_LIBRARY_PATH"; value = libPath; }
-            ];
-
-            packages = [
-              cargo
-              rust-analyzer
-              rustc
-              rustfmt
-              just
-            ];
+          depsOverrides = {
+            add-inputs.overrideAttrs = old: {
+              nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.wayland-protocols];
+              buildInputs = (old.buildInputs or []) ++ [pkgs.pkg-config pkgs.openssl.dev pkgs.openssl pkgs.perl];
+            };
           };
-
-          # export the release package of the crate as default package
-          packages.default = crateOutputs.packages.release;
-
-          # export overlay using easyOverlays
-          overlayAttrs = {
-            inherit (config.packages) wayper;
-          };
-          packages.wayper = crateOutputs.packages.release;
         };
+
+        devshells.default = with pkgs; {
+          motd = ''
+            -----------------
+            -wayper devshell-
+            -----------------
+            $(type -p menu &>/dev/null && menu)
+          '';
+          env = [
+            {
+              name = "RUST_SRC_PATH";
+              value = rustPlatform.rustLibSrc;
+            }
+            {
+              name = "LD_LIBRARY_PATH";
+              value = libPath;
+            }
+          ];
+
+          packages = [
+            cargo
+            rust-analyzer
+            rustc
+            rustfmt
+            just
+          ];
+
+          commands = [
+            {
+              name = "run-${crateName}";
+              command = "RUST_LOG=debug nix run .#${crateName}-dev";
+              help = "Run ${crateName} (debug build)";
+              category = "Run";
+            }
+            {
+              name = "run-${crateName}-rel";
+              command = "RUST_LOG=debug nix run .#${crateName}-rel";
+              help = "Run ${crateName} (release build)";
+              category = "Run";
+            }
+          ];
+        };
+
+        # export the release package of the crate as default package
+        packages.default = crateOutputs.packages.release;
+
+        # export overlay using easyOverlays
+        overlayAttrs = {
+          inherit (config.packages) wayper;
+        };
+        packages.wayper = crateOutputs.packages.release;
+      };
     };
 }
-
